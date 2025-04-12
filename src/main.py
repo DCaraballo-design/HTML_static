a2bc6e02571f5a0df48a29c16e9518748cd1e77f
DC <artistdmc@gmail.com>
2025-04-11 20:16:16 -0400

    test textnode

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from textnode import TextNode
 
 def main():
     # Create a TextNode instance
-    node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node)
 
 
