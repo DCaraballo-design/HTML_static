114ee6471d5a67babc0a2adc45ec75aca57a6b97
DC <artistdmc@gmail.com>
2025-04-12 18:45:02 -0400

    adding new test for htmlnode and updating htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,24 +1,20 @@
 class HTMLNode:
-    def __init__(self, tag=None, value=None, children=None, props=None,):
-        self.tag = tag if tag is not None else ''
-        self.value = value if value is not None else {}
-        self.children = children if children is not None else []
-        self.props = props if props is not None else {}
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
 
     def to_html(self):
-        raise NotImplementedError("Subclasses should implement this method")    
-    
-    def props_to_html(self,props):
-        return ' '.join(f'{key}="{value}"' for key, value in props.items())
-    
-    def __repr__(self):
-        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
-    
-    def add_child(self, child):
-        self.children.append(child)
+        raise NotImplementedError("to_html method not implemented")
 
-    def to_string(self):
-        attr_str = ' '.join(f'{key}="{value}"' for key, value in self.attributes.items())
-        attr_str = f' {attr_str}' if attr_str else ''
-        children_str = ''.join(child.to_string() if isinstance(child, HTMLNode) else str(child) for child in self.children)
-        return f'<{self.tag}{attr_str}>{children_str}</{self.tag}>'
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
